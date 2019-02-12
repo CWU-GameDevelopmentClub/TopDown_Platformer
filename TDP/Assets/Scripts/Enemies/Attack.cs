a980a523bde8ce28f712ab8f5a6f613cbf117bb5
Miguel Garnica <mgarnica123@aol.com>
2019-02-11 22:28:29 -0800

    Enemy Attack
    Added:
     - Attack Script
     - EnemyAI Script
     - Enemy Prefab
    Updated:
      - Player prefab

---
+++ TDP/Assets/Scripts/Enemies/Attack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    public float damage;
+    public float knockBackForce;
+    public float attackTime;
+    public KeyCode attack;
+    private BoxCollider attackBox;
+    Vector3 direction;
+    // Use this for initialization
+    void Start()
+    {
+        attackBox = GetComponent<BoxCollider>();
+    }
+
+    float timer = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(attack))
+        {
+            attackBox.enabled = true;
+        }
+
+        if (attackBox.enabled)
+        {
+            timer += Time.deltaTime;
+            if (timer > attackTime)
+            {
+                attackBox.enabled = false;
+                timer = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Health hp = collision.GetComponent<Health>();
+        if (hp == null)
+            return;
+        else
+        {
+            hp.TakeDamage(damage);
+        }
+
+        Rigidbody rb = collision.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+        else
+        {
+            direction = (collision.transform.position - this.transform.position).normalized;
+            rb.AddForce(new Vector3(direction.x, direction.y, direction.z) * knockBackForce, ForceMode.Impulse);
+        }
+
+    }
+
+    public void DoAttack()
+    {
+        attackBox.enabled = true;
+    }
+}
---
+++ TDP/Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed, walkTime, attackRate;
+    private BoxCollider attackBox;
+    private Attack attack;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        attack = GetComponentInChildren<Attack>();
+	}
+
+
+    float wTimer = 0, atTimer = 0;
+	// Update is called once per frame
+	void Update () {
+
+        wTimer += Time.deltaTime;
+        atTimer += Time.deltaTime;
+
+       if(wTimer > walkTime)
+        {
+            Walk();
+            wTimer = 0; 
+        }
+
+       if(atTimer > attackRate)
+        {
+            attack.DoAttack();
+            atTimer = 0;
+        }
+
+    }
+
+    private void Walk()
+    {
+        rb.velocity = new Vector3(speed, 0, speed) * Random.Range(-2f,2f);
+    }
+
+}
